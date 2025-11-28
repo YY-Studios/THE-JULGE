e101fe268ba4def6ab0ca7cec4eca4194d291bed
lywoo00 <lywoo00@naver.com>
2025-11-28 16:08:26 +0900

    feat: button,Link 통합 컴포넌트 제작(#17)

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,16 @@
+import Button from '@/components/common/button/Button';
+
 const Page = () => {
-  return <div>login</div>;
+  return (
+    <>
+      <Button variant="primary" size="md" as="button">
+        버튼
+      </Button>
+      <Button variant="outline" size="sm" href="/signup" as="link">
+        링크
+      </Button>
+    </>
+  );
 };
 
 export default Page;
---
+++ src/components/common/button/Button.tsx
@@ -0,0 +1,63 @@
+import clsx from 'clsx';
+import Link from 'next/link';
+import { ComponentPropsWithoutRef } from 'react';
+
+import {
+  BUTTON_BASE,
+  BUTTON_VARIANTS,
+  BUTTON_SIZES,
+  ButtonVariant,
+  ButtonSize,
+} from './buttonVariants';
+
+interface BaseButtonProps {
+  variant?: ButtonVariant;
+  size?: ButtonSize;
+}
+
+interface ButtonAsButton
+  extends BaseButtonProps,
+    ComponentPropsWithoutRef<'button'> {
+  as?: 'button';
+  href?: never;
+}
+
+interface ButtonAsLink extends BaseButtonProps, ComponentPropsWithoutRef<'a'> {
+  as?: 'link';
+  href: string;
+}
+
+type ButtonProps = ButtonAsButton | ButtonAsLink;
+
+const Button = ({
+  children,
+  variant = 'primary',
+  size = 'md',
+  className,
+  as = 'button',
+  ...props
+}: ButtonProps) => {
+  const buttonClassName = clsx(
+    BUTTON_BASE,
+    BUTTON_VARIANTS[variant],
+    BUTTON_SIZES[size],
+    className
+  );
+
+  if (as === 'link') {
+    const { href, ...linkProps } = props as ButtonAsLink;
+    return (
+      <Link href={href} className={buttonClassName} {...linkProps}>
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <button className={buttonClassName} {...(props as ButtonAsButton)}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
