f70998ed889ce4926530be963a6ff46a233762b2
lywoo00 <lywoo00@naver.com>
2025-11-28 13:05:59 +0900

    chore: axios server, client 설정(#5)

--- src/lib/axios/client.ts
+++ src/lib/axios/client.ts
@@ -1,9 +1,33 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 export const clientApi = axios.create({
   baseURL: '/api',
   timeout: 1000,
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
+clientApi.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  }
+);
+
+clientApi.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  (error: AxiosError) => {
+    if (error.response?.status === 401) {
+      if (!window.location.pathname.includes('/login')) {
+        window.location.href = '/login';
+      }
+    }
+    return Promise.reject(error);
+  }
+);
--- src/lib/axios/server.ts
+++ src/lib/axios/server.ts
@@ -1,5 +1,4 @@
-import axios from 'axios';
-
+import axios, { AxiosError } from 'axios';
 export const serverApi = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 10000,
@@ -7,3 +6,25 @@ export const serverApi = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+serverApi.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  }
+);
+
+serverApi.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  (error: AxiosError) => {
+    const status = error.response?.status;
+    if (status === 401) {
+      console.warn('사용자 인증 필요');
+    }
+    return Promise.reject(error);
+  }
+);
