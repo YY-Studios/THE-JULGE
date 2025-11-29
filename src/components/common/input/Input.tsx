44135b03518eac91788743ae84d8c585e5e50687
lywoo00 <57310334+lywoo00@users.noreply.github.com>
2025-11-29 21:41:42 +0900

    feat: input 컴포넌트 제작(#18)
    * feat: input 컴포넌트 제작(#18)

---
+++ src/components/common/input/Input.tsx
@@ -0,0 +1,47 @@
+import clsx from 'clsx';
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  error?: string;
+  suffix?: React.ReactNode;
+}
+
+const Input = ({
+  label,
+  error,
+  suffix,
+  className,
+  id,
+  type = 'text',
+  placeholder,
+  ...props
+}: InputProps) => {
+  return (
+    <div className="block">
+      {label && (
+        <label htmlFor={id} className="text-sm font-medium">
+          {label}
+        </label>
+      )}
+      <div
+        className={clsx(
+          'flex items-center bg-white border rounded-lg px-4 h-14',
+          error ? 'border-red-500' : 'border-gray-300',
+          className
+        )}
+      >
+        <input
+          type={type}
+          id={id}
+          placeholder={placeholder}
+          className="w-full outline-none"
+          {...props}
+        />
+        {suffix && <span className="text-black ml-2">{suffix}</span>}
+      </div>
+      {error && <p className="text-primary text-xs">{error}</p>}
+    </div>
+  );
+};
+
+export default Input;
